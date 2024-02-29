4ddb243d997a14451f7cce5dadf896e6cd026d22
Sergei Ivaschenko <ltfliveru3@gmail.com>
2024-02-29 22:43:36 +0300

    remove domain proj

--- src/SI.BambooCard.Domain/DynamicEntities/DynamicEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using SI.BambooCard.Domain.Entities;
-
-namespace SI.BambooCard.Domain.DynamicEntities
-{
-    public class DynamicEntity : Entity
-    {
-        public string Value { get; set; }
-    }
-}
